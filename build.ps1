2932f10a92203df8723a037d5efbd7b581afe392
kpkpkpkpkpkpkpkp <itskevp@gmail.com>
2021-12-17 02:05:56 -0600

    fixing up build to reduce repo size

--- build.ps1
+++ build.ps1
@@ -1,8 +1,22 @@
+
+Remove-Item -Path '.\game' -Recurse -Force
+New-Item -Path '.' -Name 'game' -ItemType 'directory'
+New-Item -Path '.\game' -Name 'src' -ItemType 'directory'
+New-Item -Path '.\game' -Name 'lib' -ItemType 'directory'
+New-Item -Path '.\game\lib' -Name 'HC' -ItemType 'directory'
+New-Item -Path '.\game\assets' -Name 'Backgrounds' -ItemType 'directory'
+New-Item -Path '.\game\assets' -Name 'fonts' -ItemType 'directory'
+New-Item -Path '.\game\assets' -Name 'sounds' -ItemType 'directory'
+New-Item -Path '.\game\assets\sounds' -Name 'old' -ItemType 'directory'
+New-Item -Path '.\game\assets' -Name 'sprites' -ItemType 'directory'
+New-Item -Path '.\game\assets\sprites' -Name 'Updated Assets' -ItemType 'directory'
+New-Item -Path '.\game\assets\sprites\Updated Assets' -Name 'Backgrounds' -ItemType 'directory'
+
 Copy-Item -Force ".\main.lua" -Destination ".\game\main.lua"
 Copy-Item -Force ".\src\*" -Destination ".\game\src" -Recurse
 Copy-Item -Force ".\assets\*" -Destination ".\game\assets" -Recurse
 Copy-Item -Force ".\lib\*" -Destination ".\game\lib" -Recurse
-
+Copy-Item -Force ".\love\*" -Destination ".\game"
 
 $compress = @{
   Path = ".\game\main.lua",".\game\src",".\game\assets",".\game\lib"
